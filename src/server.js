5e64b996621edfdbdf87e25f8ef81fef34f06b98
ChristianCan <crisrojo9@hotmail.com>
2021-06-23 01:01:32 -0500

    fix cors 2

--- src/server.js
+++ src/server.js
@@ -29,6 +29,24 @@ app.use(
   cors({ origin: "https://starmoviesreact.netlify.app/", credentials: true })
 );
 app.options("https://starmoviesreact.netlify.app/", cors());
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://starmoviesreact.netlify.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(
   session({
     secret: "mysecretsession",
